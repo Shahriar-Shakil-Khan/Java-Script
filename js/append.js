9824aa8d15af9c60928add6afdd1d3b9a44a45c1
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-05-06 10:21:44 +0600

    shakil

---
+++ js/append.js
@@ -0,0 +1,31 @@
+console.log('append js')
+
+const placeList=document.getElementById('place-list');
+const li =document.createElement('li');
+li.innerText='RangaMati';
+placeList.appendChild(li);
+
+const mainContainer=document.getElementById('main-container');
+const section=document.createElement('section');
+const h1=document.createElement('h1');
+h1.innerText='My Food List';
+section.appendChild(h1);
+
+const ul=document.createElement('ul');
+const li1=document.createElement('li');
+li1.innerText='Biriyani'
+ul.appendChild(li1);
+
+const li2=document.createElement('li');
+li2.innerText='Cake'
+ul.appendChild(li2);
+
+const li3=document.createElement('li');
+li3.innerText='Rice'
+ul.appendChild(li3);
+
+section.appendChild(ul)
+mainContainer.appendChild(section);
+
+
+
---
+++ js/dom.js
@@ -0,0 +1,3 @@
+console.log('Hello From JS');
+console.log(document);
+console.log(document.body);
